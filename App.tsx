2f222af29f7d0f2534054e7dc6a669abd7f3f85b
Breno Nathanael <breno_nathanael@hotmail.com>
2023-07-17 15:21:50 -0300

    update and context in app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 
 import 'react-native-gesture-handler';
+import { InfoProvider } from './src/context';
 import { StackRouter } from './src/screens/Register';
 
 export default function App() {
   return (
-    <>
+    <InfoProvider>
       <StackRouter />
       <StatusBar style='light' />
-    </>
+    </InfoProvider>
   );
 }
